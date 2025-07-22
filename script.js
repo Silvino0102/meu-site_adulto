383492bf3866a94c9aff3b91beb441bae7dccaa0
Silvino0102 <silvinosilver0001@gmail.com>
2025-07-23 00:30:23 +0100

    index.html
    index.html

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
 javascript
-document.getElementById('loginBtn').addEventListener('click', () => {
-  alert('Login ainda não implementado.');
-});
-
-document.getElementById('signupBtn').addEventListener('click', () => {
-  alert('Redirecionando para a página de pagamento...');
-});
-document.getElementById('joinNow').addEventListener('click', () => {
-  alert('Assine para desbloquear conteúdo.');
-});
+function liberarAcesso() {
+  document.getElementById("menu").style.display = "block";
+}
